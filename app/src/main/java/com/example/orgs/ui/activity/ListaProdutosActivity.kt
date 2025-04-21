87afe1f625371b1582c240f886714c36a78d3ac6
matheus <matheus.malmeida7@gmail.com>
2025-04-21 17:49:31 -0300

    add

---
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,51 @@
+package com.example.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaProdutosActivity : AppCompatActivity() {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setContentView(R.layout.activity_lista_produtos)
+        configuraRecyclerView()
+        configuraFab()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+    }
+
+    private fun configuraFab() {
+        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produtos_floatingActionButton)
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.activity_lista_produtos_reciclerView)
+        val dao = ProdutosDao()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
